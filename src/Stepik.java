85d8620cb6e61549d975bbcb1814414cd6e1bf16
Nasibulin <k.nasibulin@yandex.ru>
2018-08-20 11:59:27 +0300

    -

---
+++ src/Stepik.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 20.08.18
+ * Time: 12:57
+ * To change this template use File | Settings | File Templates.
+ */
+public class Stepik {
+
+
+    static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+
+}
