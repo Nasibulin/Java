8d92e4b9d01a11dc0f34704b94f5ebd367170e83
Nasibulin <k.nasibulin@yandex.ru>
2018-08-20 12:57:22 +0300

    -

--- src/Stepik.java
+++ src/Stepik.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Nasibulin
@@ -8,11 +10,45 @@
 public class Stepik {
 
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
-        System.out.println("Hello World!");
-        System.out.println("Testing repository...");
+//        System.out.println("Hello World!");
+//        System.out.println("Testing repository...");
+
+        String [] roles= {"Городничий","Аммос Федорович","Артемий Филиппович","Лука Лукич"};
+        String [] textLines={"Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор.",
+                "Аммос Федорович: Как ревизор?",
+                "Артемий Филиппович: Как ревизор?",
+                "Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем.",
+                "Аммос Федорович: Вот те на!",
+                "Артемий Филиппович: Вот не было заботы, так подай!",
+                "Лука Лукич: Господи боже! еще и с секретным предписаньем!"};
+
+        System.out.println(printTextPerRole(roles,textLines));
 
     }
+    private static String printTextPerRole(String[] roles, String[] textLines) {
+        StringBuilder answer = new StringBuilder();
+        String subString = new String();
+        for (String role : roles) {
+            answer.append( role +":\n" );
+            for (String textLine : textLines) {
+                if (textLine.startsWith( role + ":" )) {
+                    subString = textLine.substring( role.length()+2 );
+//                    answer.append(Arrays.asList( textLines ).indexOf(textLine) + 1).append(") ").append(subString).append( "\n" );
+                    answer.append(Arrays.asList( textLines ).indexOf(textLine) + 1).append(") ").append(subString).append( "\n" );
 
+                }
+            }
+            answer.append( "\n" );
+
+            //.append(Arrays.asList( textLines ).indexOf(textLine) + 1)
+            //заменил на счетчик i++
+            //.append(i)
+
+
+        }
+
+        return answer.toString();
+    }
 }
