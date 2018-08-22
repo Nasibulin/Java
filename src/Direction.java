9a4043d150b8b0b1ee4a337d73c5f9ef189bd959
Nasibulin <k.nasibulin@yandex.ru>
2018-08-22 21:20:08 +0300

    -

---
+++ src/Direction.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Konstantin
+ * Date: 22.08.18
+ * Time: 20:24
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
