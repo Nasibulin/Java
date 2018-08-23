8a6c858f04a9f0c3869b7a9c67ee9e50e02aaa9f
Nasibulin <k.nasibulin@yandex.ru>
2018-08-23 13:52:10 +0300

    Задачка про робота )

---
+++ src/Robot.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 23.08.18
+ * Time: 13:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class Robot {
+}
