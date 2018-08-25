9149192a8638801ff5c72c93a355f41fd2b78ba1
Nasibulin <k.nasibulin@yandex.ru>
2018-08-25 09:07:00 +0300

    Переопределение equals и hashcode

---
+++ src/ComplexNumber.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Konstantin
+ * Date: 25.08.18
+ * Time: 8:23
+ * To change this template use File | Settings | File Templates.
+ */
+public class ComplexNumber {
+}
