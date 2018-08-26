0ad064f9263293779dbc06d85b930a10b24c2c3f
Nasibulin <k.nasibulin@yandex.ru>
2018-08-26 18:17:16 +0300

    Интерфейсы

---
+++ src/AsciiCharSequence.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Konstantin
+ * Date: 26.08.18
+ * Time: 15:09
+ * To change this template use File | Settings | File Templates.
+ */
+public class AsciiCharSequence {
+}
