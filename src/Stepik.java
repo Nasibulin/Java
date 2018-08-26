651ade8034d4fc112b9424042b3b8bb86380cade
Nasibulin <k.nasibulin@yandex.ru>
2018-08-26 18:18:57 +0300

    -

--- src/Stepik.java
+++ src/Stepik.java
@@ -13,21 +13,22 @@ public class Stepik {
 
     public static void main(String[] args) {
 
-//        System.out.println("Hello World!");
-//        System.out.println("Testing repository...");
+        byte[] example = {72, 101, 108, 108, 111, 33};
+        AsciiCharSequence answer = new AsciiCharSequence(example);
+        System.out.println("Последовательность - " + answer.toString());//Hello!
+        System.out.println("Размер её - " + answer.length());//6
+        System.out.println("Символ под № 1 - " + answer.charAt(1));//e
+        System.out.println("Подпоследовательность - " + answer.subSequence(1, 5));//ello
+//проверка на нарушение инкапсуляции private поля
+        System.out.println(answer.toString());//Hello!
+        example[0] = 74;
+        System.out.println(answer.toString());//Hello!
 
-        String [] roles= {"Городничий","Аммос Федорович","Артемий Филиппович","Лука Лукич"};
-        String [] textLines={"Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор.",
-                "Аммос Федорович: Как ревизор?",
-                "Артемий Филиппович: Как ревизор?",
-                "Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем.",
-                "Аммос Федорович: Вот те на!",
-                "Артемий Филиппович: Вот не было заботы, так подай!",
-                "Лука Лукич: Господи боже! еще и с секретным предписаньем!"};
 
-        System.out.println(printTextPerRole(roles,textLines));
 
     }
+
+
     private static String printTextPerRole(String[] roles, String[] textLines) {
         StringBuilder answer = new StringBuilder();
         String subString;
@@ -42,6 +43,7 @@ public class Stepik {
                  }
              i++;
             }
+
             answer.append( "\n" );
         }
         return answer.toString();
