1af6de192a042e3bdeae0e614c88ad8a32937203
Nasibulin <k.nasibulin@yandex.ru>
2018-08-29 16:00:39 +0300

    -

--- sql-ex/pass_income_days.sql
+++ sql-ex/pass_income_days.sql
@@ -1,15 +1,21 @@
 WITH s1
-     AS (SELECT  id_psg
+     AS (SELECT id_psg,
+                day,
+                count (day) cnt1,
+                cnt
            FROM (SELECT id_psg,
                         day,
                         count (day) OVER (PARTITION BY id_psg) cnt
-                   FROM (SELECT id_psg,DATEPART (dw, date) day
+                   FROM (SELECT id_psg, DATEPART (dw, date) day
                            FROM Pass_in_trip) s) s
          GROUP BY id_psg, day, cnt
-         HAVING    (    day IN (DATEPART (dw, '20180825'), DATEPART (dw, '20180826'))
+         HAVING    (    day IN
+                           (DATEPART (dw, '20180825'),
+                            DATEPART (dw, '20180826'))
                     AND count (day) * 1.0 / cnt <
                            (cnt - count (day)) * 1.0 / cnt)
-                OR (day NOT IN (DATEPART (dw, '20180825'), DATEPART (dw, '20180826')))),
+                OR (day NOT IN
+                       (DATEPART (dw, '20180825'), DATEPART (dw, '20180826')))),
      s2
      AS (SELECT id_psg, day_in, (count (day_in) * 1.0 / cnt) freq
            FROM (SELECT id_psg,
@@ -17,15 +23,24 @@ WITH s1
                         count (day_in) OVER (PARTITION BY day_in) cnt
                    FROM (SELECT id_psg,
                                 datepart (
-                                      dw,
-                                      date + iif (time_out < time_in, 0, 1)) day_in
+                                   dw,
+                                   date + iif (time_out < time_in, 0, 1))
+                                   day_in
                            FROM trip
                                 JOIN Pass_in_trip
                                    ON Pass_in_trip.trip_no = Trip.trip_no
                           WHERE id_psg IN (SELECT id_psg FROM s1)) s) s
          GROUP BY id_psg, day_in, cnt
-         HAVING day_in = DATEPART (dw, '20180820')
-         )
+         HAVING day_in = DATEPART (dw, '20180820'))
 SELECT name
-  FROM s2 JOIN passenger p ON s2.id_psg = p.id_psg
- WHERE freq = (SELECT max (freq) FROM s2)
+  FROM (SELECT s1.id_psg
+          FROM s1
+         WHERE NOT EXISTS (SELECT * FROM s2)
+        UNION
+        SELECT s2.id_psg
+          FROM s2
+         WHERE (   freq > (SELECT max (freq)
+                             FROM s2
+                            WHERE id_psg <> s2.id_psg)
+                OR (SELECT count (DISTINCT freq) FROM s2) = 1)) x
+       JOIN Passenger p ON p.id_psg = x.id_psg
