eba59bfd5cc1e045cd392fb463836ae4617ac7b7
Nasibulin <k.nasibulin@yandex.ru>
2018-08-29 15:59:52 +0300

    Задачка на абстрактные методы и интерфейсы

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 29.08.18
+ * Time: 12:34
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
