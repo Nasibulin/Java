e917ddcd8f552f027e999a205579a168b7588997
Nasibulin <k.nasibulin@yandex.ru>
2018-08-31 13:13:08 +0300

    -

--- src/bookpack/Book.java
+++ src/bookpack/Book.java
@@ -7,5 +7,25 @@ package bookpack;
  * Time: 14:03
  * To change this template use File | Settings | File Templates.
  */
+// Объявление защищенными переменных экземпляра в классе Book,
 public class Book {
+    // При объявлении этих переменных использован
+    // модификатор доступа protected.
+    protected String title;
+    protected String author;
+    protected int pubDate;
+
+    public Book(String t, String a, int d) {
+        title = t;
+        author = a;
+        pubDate = d;
+    }
+
+    public void show() {
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(pubDate);
+        System.out.println() ;
+    }
 }
+
