6d7571285fca8b70772a560fc8e8a292098a9a3b
Nasibulin <k.nasibulin@yandex.ru>
2018-08-31 10:22:31 +0300

    -

---
+++ src/AbsShape.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 31.08.18
+ * Time: 10:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class AbsShape {
+}
