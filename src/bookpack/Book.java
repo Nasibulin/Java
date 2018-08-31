a9be899f18f78c420a3de26d6c89c6d68df0f8f6
Nasibulin <k.nasibulin@yandex.ru>
2018-08-31 13:08:30 +0300

    -

---
+++ src/bookpack/Book.java
@@ -0,0 +1,11 @@
+package bookpack;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 31.08.18
+ * Time: 14:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class Book {
+}
---
+++ src/bookpackext/ProtectDemo.java
@@ -0,0 +1,11 @@
+package bookpackext;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 31.08.18
+ * Time: 14:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProtectDemo {
+}
